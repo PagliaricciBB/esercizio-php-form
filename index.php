06aa980cd1c97ba5eda3e9dd5d6e65d9de143358
dani128 <michielin.daniel@gmail.com>
2019-02-27 10:54:02 +0100

    ciao

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php 
+$arr=["kiwi","pera","mela"];
+for ($i=0;$i<count($arr);i++){
+  print $arr [$i]
+};
+ ?>
+ <!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+<!--style.css-->
+    <link rel="stylesheet" href="style.css">
+    
+
+    <tittle> Verifica Grid Michielin Daniel </tittle>
+    </head>
+    <body>
+      
+
+
+
+
+
+
+
+
+
+
+    </body>
+     <script src="js/jquery-3.3.1.slim.min.js"></script>
+    <script src=js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html
